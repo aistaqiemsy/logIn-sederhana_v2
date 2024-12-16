72d0d2d4d7ae097a29dd004e53255ee4047990f5
aistaqiemsy <a.istakim@gmail.com>
2024-12-16 11:44:33 +0700

    aplikasi log in sederhana HTML-PHP v2

---
+++ halamanKedua.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Autentikasi Berhasil</title>
+</head>
+<body>
+    <center>
+        Halaman Berhasil Log In!
+        <hr>
+        <a href="logIn_v2.php"><b>Keluar</b></a>
+    </center>
+</body>
+</html>
